86ad76d457ae1d2ad93d86f988cfc90715b731f7
Chris Nelson <superchrisnelson@gmail.com>
2016-02-09 13:41:37 -0500

    routing to edit with form

--- app/app.ts
+++ app/app.ts
@@ -1,5 +1,6 @@
 import {Component} from 'angular2/core';
 import CandidateDetails from "./candidate-details";
+import CandidateEdit from "./candidate-edit";
 import CandidateService from "./candidate-service";
 import {ROUTER_DIRECTIVES, RouteConfig} from "angular2/router";
 
@@ -10,7 +11,8 @@ import {ROUTER_DIRECTIVES, RouteConfig} from "angular2/router";
     directives: [CandidateDetails, ROUTER_DIRECTIVES]
 })
 @RouteConfig([
-  {path: "/candidate/:id", name: "CandidateDetails", component: CandidateDetails}
+  {path: "/candidate/:id", name: "CandidateDetails", component: CandidateDetails},
+  {path: "/candidate/:id/edit", name: "CandidateEdit", component: CandidateEdit}
 ])
 export default class AppComponent {
   constructor(candidateService: CandidateService) {
--- app/candidate-edit.ts
+++ app/candidate-edit.ts
@@ -1,13 +1,12 @@
 import {Component, Input} from 'angular2/core';
 import CandidateService from "./candidate-service";
-import {RouteParams, ROUTER_DIRECTIVES} from "angular2/router";
+import {RouteParams} from "angular2/router";
 
 @Component({
   selector: 'candidate-details',
-  templateUrl: "app/candidate-details.html",
-  directives: [ROUTER_DIRECTIVES]
+  templateUrl: "app/candidate-edit.html"
 })
-export default class CandidateDetailsComponent {
+export default class CandidateEditComponent {
   constructor(routeParams: RouteParams, candidateService: CandidateService) {
     this.candidateService = candidateService;
     this.candidateService.getCandidate(routeParams.get("id")).subscribe( (candidate) => {
