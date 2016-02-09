784576dec0232db198d18b69225ef936bec5f240
Chris Nelson <superchrisnelson@gmail.com>
2016-02-09 15:35:00 -0500

    save works without updating list view

--- app/candidate-edit.ts
+++ app/candidate-edit.ts
@@ -1,16 +1,24 @@
 import {Component, Input} from 'angular2/core';
 import CandidateService from "./candidate-service";
-import {RouteParams} from "angular2/router";
+import {RouteParams, Router} from "angular2/router";
 
 @Component({
   selector: 'candidate-details',
   templateUrl: "app/candidate-edit.html"
 })
 export default class CandidateEditComponent {
-  constructor(routeParams: RouteParams, candidateService: CandidateService) {
+  constructor(routeParams: RouteParams, candidateService: CandidateService, router: Router) {
     this.candidateService = candidateService;
+    this.router = router;
     this.candidateService.getCandidate(routeParams.get("id")).subscribe( (candidate) => {
       this.candidate = candidate;
+    });
+  }
+
+  saveCandidate() {
+    this.candidateService.saveCandidate(this.candidate).subscribe((savedCandidate) => {
+      this.router.parent.navigate(["CandidateDetails", {id: savedCandidate.id}])
+      console.log(savedCandidate);
     });
   }
 }
--- app/candidate-service.ts
+++ app/candidate-service.ts
@@ -1,5 +1,5 @@
 import { Inject } from "angular2/core";
-import { Http } from "angular2/http";
+import { Http, Headers } from "angular2/http";
 import "rxjs/Rx";
 
 @Inject()
@@ -14,6 +14,12 @@ export default class CandidateService {
 
   getCandidate(id) {
     return this.http.get(`http://localhost:8000/candidates/${id}`).map( (response) => response.json());
+  }
+
+  saveCandidate(candidate) {
+    let headers = new Headers();
+    headers.append("Content-Type", "application/json");
+    return this.http.put(`http://localhost:8000/candidates/${candidate.id}`, JSON.stringify(candidate), {headers: headers}).map( (response) => response.json());
   }
 
 }
